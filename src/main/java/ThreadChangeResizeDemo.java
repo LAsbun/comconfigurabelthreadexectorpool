df5f115a73ee74f479e57cecf5ea1f838b515394
LAsbun <swsgoog@gmail.com>
2020-07-07 22:31:16 +0800

    feat:resizableCapacityBlockQueue

---
+++ src/main/java/ThreadChangeResizeDemo.java
@@ -0,0 +1,66 @@
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import configurablethreadpool.ResizableCapacityLinkedBlockQueue;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by sws
+ * <p>
+ * demo from https://mp.weixin.qq.com/s/EhBt44Rj0c5E-UVf742aGw")
+ */
+
+public class ThreadChangeResizeDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        dynamicModeifuExector();
+    }
+
+    private static ThreadPoolExecutor buildTHreadPoolExecutor() {
+        return new ThreadPoolExecutor(2, 5, 60, TimeUnit.SECONDS, new ResizableCapacityLinkedBlockQueue<>(10),
+                new ThreadFactoryBuilder().setNameFormat("ThreadChangeDemo").build());
+    }
+
+    private static void dynamicModeifuExector() throws InterruptedException {
+        ThreadPoolExecutor executor = buildTHreadPoolExecutor();
+        for (int i = 0; i < 15; i++) {
+            executor.submit(() -> {
+                threadPoolStatus(executor, "创建任务");
+                try {
+                    TimeUnit.SECONDS.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        threadPoolStatus(executor, "改变之前");
+        executor.setCorePoolSize(10);
+        executor.setMaximumPoolSize(10);
+        ResizableCapacityLinkedBlockQueue queue = (ResizableCapacityLinkedBlockQueue) executor.getQueue();
+        queue.setCapacity(100);
+        threadPoolStatus(executor, "改变之后");
+        Thread.currentThread().join();
+
+    }
+
+    private static void threadPoolStatus(ThreadPoolExecutor executor, String name) {
+        ResizableCapacityLinkedBlockQueue queue = (ResizableCapacityLinkedBlockQueue) executor.getQueue();
+        System.out.println(Thread.currentThread().getName() + "-" + name + "-:" +
+                "核心线程数:" + executor.getCorePoolSize() +
+                " 活动线程数:" + executor.getActiveCount() +
+                " 最大线程数:" + executor.getMaximumPoolSize() +
+                " 线程池活跃度:" + diveide(executor.getActiveCount(), executor.getMaximumPoolSize()) +
+                " 任务完成数:" + executor.getCompletedTaskCount() +
+                " 队列大小:" + (queue.size() + queue.remainingCapacity()) +
+                " 当前排队线程数:" + queue.size() +
+                " 队列剩余大小:" + queue.remainingCapacity()
+
+        );
+    }
+
+    private static String diveide(int num1, int num2) {
+        return String.format("%1.2f%%", Double.parseDouble(num1 + "")
+                / Double.parseDouble(num2 + ""));
+    }
+}
